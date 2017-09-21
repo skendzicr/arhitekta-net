84699d2d4adff4d14ab2b9bbd734c4574f62cd01
Radovan Skendzic <rskendzic@execom.eu>
2017-09-21 17:51:57 +0200

    added previous and next buttons for single projects

--- src/app/components/single-project/single-project.component.ts
+++ src/app/components/single-project/single-project.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Project } from '../../shared/project';
 import { ProjectsService } from '../../shared/projects.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gallery';
 import { GALLERY_OPTIONS } from '../../shared/gallery.constants';
+import 'rxjs/add/observable/of';
+
+import 'rxjs/add/operator/switchMap';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-single-project',
@@ -12,6 +16,9 @@ import { GALLERY_OPTIONS } from '../../shared/gallery.constants';
 })
 export class SingleProjectComponent implements OnInit {
   project: Project;
+  slugs;
+  prev: number;
+  next: number;
   galleryOptions: NgxGalleryOptions[];
   galleryImages: NgxGalleryImage[];
   constructor(private projectsService: ProjectsService, private route: ActivatedRoute, ) { }
@@ -20,6 +27,30 @@ export class SingleProjectComponent implements OnInit {
     this.project = this.projectsService.getProject(this.route.snapshot.paramMap.get('slug'));
     this.galleryOptions = GALLERY_OPTIONS;
     this.galleryImages = this.project.images;
+    this.slugs = this.projectsService.getSlugs();
+
+    this.route.params.switchMap((params: Params) => {
+      return Observable.of(this.projectsService.getProject(params.slug));
+    }).subscribe(project => {
+      this.project = project;
+      this.galleryImages = project.images;
+      this.setPrevNext(project.id);
+    });
+
+  }
+
+  setPrevNext(projectId: number) {
+    let prevIndex = projectId;
+    let nextIndex = projectId;
+    if (projectId < 1) {
+      prevIndex = this.slugs.length;
+      nextIndex = 1;
+    } else if (projectId === this.slugs.length - 1 ) {
+      nextIndex = -1;
+    }
+
+    this.prev = this.slugs[prevIndex - 1].slug;
+    this.next = this.slugs[nextIndex + 1].slug;
   }
 
 }
--- src/app/shared/projects.service.ts
+++ src/app/shared/projects.service.ts
@@ -1,17 +1,24 @@
 import { Injectable } from '@angular/core';
 import { PROJECTS } from './projects.const';
 import { Project } from './project';
-
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class ProjectsService {
-  constructor() {}
+  constructor() { }
   getProjects() {
     this.shuffleArray(PROJECTS);
     return PROJECTS;
   }
   getProject(slug: string): Project {
     return PROJECTS.filter(project => project.slug === slug)[0];
+  }
+
+  getSlugs() {
+    return PROJECTS.map(projects => {
+      return {id: projects.id, slug: projects.slug};
+    }
+    );
   }
 
   shuffleArray = arr => arr.sort(() => (Math.random() - 0.5));
